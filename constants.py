1b6d79349574125d55a7435265ef80962e630a1e
Reace Hudgeons <rhudgeons88@gmail.com>
2016-03-25 14:36:18 -0500

    Added files via upload

---
+++ constants.py
@@ -0,0 +1,7 @@
+START_HTML_TAG = "<html>\n"
+END_HTML_TAG = "</html>\n"
+CHAR_META_TAG = "<meta charset=\"utf-8\">"
+
+START_WOD_TITLE_TAG = "<h3>"
+END_WOD_TITLE_TAG = "</h3>\n"
+
---
+++ oly_program.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+from constants import *
+
+# open files for program
+document = BeautifulSoup(open("Oly_Program.html"))
+wod_doc_temp = open("temp_wods.html", "r+")
+wod_doc_final = open("extracted_wods.html", 'w')
+
+wod_doc_final.write(START_HTML_TAG)
+wod_doc_final.write(CHAR_META_TAG)
+
+# get all div tags that contain the workout lists
+tags_to_delete = document.find_all(['span', 'a', 'br'])
+divs = document.find_all('div', { 'class' : 'workouts_list_text'} )
+
+day_num = 0
+
+# remove all span, a, and br tags and their content
+for delete in tags_to_delete:
+	delete.replaceWith('')
+
+# write workout divs into temp file
+for div in divs:
+	day_num += 1
+
+	wod_doc_temp.write(START_WOD_TITLE_TAG + "Day %i" % day_num + END_WOD_TITLE_TAG)
+	wod_doc_temp.write(div.prettify('utf-8'))
+
+wod_doc_temp.close()
+wod_doc_temp = open("temp_wods.html", "r")
+
+day_num = 0
+
+# write workouts into final html file
+for line in wod_doc_temp:
+	if "&amp;" in line:
+		wod_doc_final.write(line.replace("&amp;", "and"))
+	elif "workouts_list_text" in line:
+		day_num += 1
+		wod_doc_final.write(line.replace("workouts_list_text", "day%i" % day_num))
+	else:
+		wod_doc_final.write(line)
+
+wod_doc_final.write(END_HTML_TAG)
+wod_doc_final.close()
